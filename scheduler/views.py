f9eb9a8e1e0167ba5b31e0e3f32f83ee8b078b29
abdullahaaf1 <abdullah.amin@majoo.id>
2023-02-19 05:28:11 +0700

    add celery worker to queue email send time

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,12 +1,19 @@
+import pytz
+from datetime import datetime
 from flask import Blueprint,request
 from flask_mail import Message
 from emailconfig.emailconfig import mail
+from celery import Celery
+from decouple import config
 
 from .models import EmailScheduler, EventParticipants
 from database.database import db
 
+celery_app = Celery('views', broker=config('CELERY_BROKER_URL'), backend=config('CELERY_BACKEND_URL'))
 scheduler = Blueprint('scheduler', __name__)
 
+
+@celery_app.task
 def send_email(event_id, body, subject):
     emails = EventParticipants.query.filter(EventParticipants.event_id == event_id).all()
     list_email = []
@@ -29,5 +36,11 @@ def save_emails():
     db.session.add(email_scheduler)
     db.session.commit()
 
-    send_email(data['event_id'], data['email_content'], data['email_subject'])
+    send_time_str = data['timestamp']
+    send_time = datetime.strptime(send_time_str, "%Y-%m-%d %H:%M:%S")
+    local_tz = pytz.timezone('Asia/Jakarta')
+    local_dt = local_tz.localize(send_time)
+    utc_dt = local_dt.astimezone(pytz.utc)
+    send_email.apply_async(args=[data['event_id'], data['email_content'], data['email_subject']], eta=utc_dt)
+
     return {"data" : data}
